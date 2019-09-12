416aac0881b6d584256cfe727cedf360f55a4f8b
Elad Stern <elad.stern@sealights.io>
2019-09-12 12:07:15 +0300

    Added QR

--- src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
@@ -6,7 +6,7 @@ public class MultiplicationDoubler implements IntegerDoubler {
 
   @Override
   public long doDouble(int i) {
-    return i * 4;
+    return i * 5;
   }
 
   public long doFloat(int i) { return i / 2; }
