707ab7c6c0da2af3f16bb98fe62538d257a3fc02
Elad Stern <elad.stern@sealights.io>
2019-09-25 11:05:32 +0300

    B27

--- src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
@@ -6,7 +6,7 @@ public class MultiplicationDoubler implements IntegerDoubler {
 
   @Override
   public long doDouble(int i) {
-    return i * 26;
+    return i * 27;
   }
 
   public long doBloat(int i) { return i / 2; }
